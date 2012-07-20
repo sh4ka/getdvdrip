8bc08bcef11a9aefe67479819a9a3e8cf3830a5e
Jesus Flores <apunta666@gmail.com>
2012-07-20 12:17:18 +0200

    F removed api from configuration file, duh!

--- configuration.py
+++ configuration.py
@@ -1,5 +1,3 @@
-API = 'tu9cdwby75hq8aacu3secfj9'
-
 # rt api
 DVDRELEASESURL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json?apikey='
 DVDPAGELIMIT = '&page_limit=50'
